d3bf6e897e0bb70be8405bc7cd28c3793bd418fd
leoperez55 <89830131+leoperez55@users.noreply.github.com>
2022-11-16 17:39:34 -0600

    added all courses.txt's

--- TranscriptReader.java
+++ TranscriptReader.java
@@ -4,46 +4,60 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TranscriptReader
 {
    public static void main(String[] args) throws IOException 
    {
-      File f1=new File("TSRPT_Sample1.txt"); //Creation of File Descriptor for input file
-      String[] words=null;  //Intialize the word Array
 
-      FileReader fr = new FileReader(f1);  //Creation of File Reader object
-      BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
-
-      String s;     
-   
       List<String> searchTerms = new ArrayList<String>();
 
+      //Reads in searchTerms were looking for from searchTerms.TXT and stores them in an ArrayList
       try (BufferedReader br2 = new BufferedReader(new FileReader("searchTerms.txt"))) {
-      String line;
-      while ((line = br2.readLine()) != null) {
-       searchTerms.add(line);
+         String line;
+         while ((line = br2.readLine()) != null) {
+          searchTerms.add(line);
+            }
          }
-      }
+
+      //Initializing file reading for the transcipt
+      File f1=new File("TSRPT_Sample1.txt"); //Creation of File Descriptor for input file
+      String[] words=null;  //Intialize the word Array
+      FileReader fr = new FileReader(f1);  //Creation of File Reader object
+      BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
+      
+      String s;     
+      int nameCntr = 0;
+
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter students Track: ");
+
+      // String input
+      String track = sc.nextLine();
+
 
       while((s=br.readLine())!=null)   //Reading Content from the file until theres no more to read
       {
+         //Gets the students name from transcipt
+         if( s.contains("Name:") && (nameCntr != 1)){
+            System.out.print(String.format("%-50s %s" , s, " "));
+            //System.out.println(s+'\n');
+            nameCntr = 1;
+         }
          
-         words=s.split(" ");  //Split the words stored in the buffer using a space 
-         for (String word : words)  //Goes through every word in the buffer and sets it equal to "word" as it parses
-            {
-                 //Compares every word in the transcipt to every word in our searchTerms.txt
-                 //to check if a class is within the transcipt
-                 int i = 0;
-                 for(; i < searchTerms.size(); i++)   
-                 {
-                   if(word.equals(searchTerms.get(i))){
-                     System.out.println(searchTerms.get(i));
-                   }
-                  
-                 }
-                 
-            }
+         if( s.contains("Student ID:")){
+            System.out.print(s+'\n');
+            //System.out.print('\n');
+
+            System.out.print(String.format("%-50s %s" , "Plan: Master", " "));
+            System.out.println("Major: Computer Science");
+            System.out.print(String.format("%-51s %s" , " ", "Track:" + track));
+         }
+
+   
+
+
       }
       
          fr.close(); //Close the File Reader
