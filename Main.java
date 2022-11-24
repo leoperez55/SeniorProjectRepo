d794dc13b0c33010c05172ea7b515d8f80232391
Hiba Khan <hibby.love@gmail.com>
2022-11-24 04:53:19 -0600

    Changes 11/23

--- Main.java
+++ Main.java
@@ -1,10 +1,25 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class Main{
 
     public static void main(String[] args) {
     
      // JComboBox = A component that combines a button or editable field and a drop-down list
      
-     new MyFrame();
+     SwingUtilities.invokeLater(new Runnable() {
+        public void run()
+        {
+            new MyFrame();
+        }
+    });
+     
    
     }
    }
+
+
+
+   
--- MyFrame.java
+++ MyFrame.java
@@ -9,31 +9,57 @@ public class MyFrame extends JFrame implements ActionListener{
  MyFrame(){
   this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   this.setLayout(new FlowLayout()); 
+
+  // ADDED CODE FOR TITLE 
+  JFrame frame = new JFrame();
+  JPanel mainPanel = new JPanel();
+  JPanel buttonsPanel = new JPanel();
+  frame.add(mainPanel);
+  frame.add(buttonsPanel, BorderLayout.SOUTH);
   
-  String[] animals = {"dog","cat","bird"};
+  String[] tracks = {"Traditional Computer Science", "Networks and Telecommunications", "Intelligent Systems", "Cyber Security", "Interactive Computing", "Systems", "Data Science", "Software Engineering"};
   
-  comboBox = new JComboBox(animals);
+  comboBox = new JComboBox(tracks);
+  comboBox.setRenderer(new MyComboBoxRenderer("Please select your track:"));
   comboBox.addActionListener(this);
+  comboBox.setSelectedIndex(-1); //By default it selects first item, we don't want any selection
+  mainPanel.add(comboBox);
   
-  //comboBox.setEditable(true);
-  //System.out.println(comboBox.getItemCount());
-  //comboBox.addItem("horse");
-  //comboBox.insertItemAt("pig", 0);
-  //comboBox.setSelectedIndex(0);
-  //comboBox.removeItem("cat");
-  //comboBox.removeItemAt(0);
-  //comboBox.removeAllItems();
-  
+
   this.add(comboBox);
   this.pack();
   this.setVisible(true);
+
+  if(comboBox.getSelectedItem() != null && comboBox.getSelectedItem().equals( "Traditional Computer Science"))
+{
+   System.out.print("Traditional Computer Science Selected");
+}
  }
  
  @Override
  public void actionPerformed(ActionEvent e) {
   if(e.getSource()==comboBox) {
    System.out.println(comboBox.getSelectedItem());
-   //System.out.println(comboBox.getSelectedIndex());
+   
   }
  }
+}
+
+class MyComboBoxRenderer extends JLabel implements ListCellRenderer
+{
+    private String _title;
+
+    public MyComboBoxRenderer(String title)
+    {
+        _title = title;
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value,
+            int index, boolean isSelected, boolean hasFocus)
+    {
+        if (index == -1 && value == null) setText(_title);
+        else setText(value.toString());
+        return this;
+    }
 }
